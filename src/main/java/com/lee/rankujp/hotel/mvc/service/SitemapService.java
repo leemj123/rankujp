e62d87c27c4498594c9b83b3333bad65f4771fcf
leemj <leemj@odongdev.com>
2025-09-25 15:41:36 +0900

    이미지 슬라이더 제외완료

--- src/main/java/com/lee/rankujp/hotel/mvc/service/SitemapService.java
+++ src/main/java/com/lee/rankujp/hotel/mvc/service/SitemapService.java
@@ -1,14 +1,14 @@
-package com.lee.rankujp.hotel.mvc.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class SitemapService {
-
-
-    public String makeSiteMap() {
-
-    }
-}
+//package com.lee.rankujp.hotel.mvc.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class SitemapService {
+//
+//
+//    public String makeSiteMap() {
+//
+//    }
+//}
