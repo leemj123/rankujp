983313bb9c46ee4065ab5a22eae5bc731ec98ac0
leemj <leemj@odongdev.com>
2025-09-25 11:13:38 +0900

    seo 마무리

---
+++ src/main/java/com/lee/rankujp/hotel/mvc/service/SitemapService.java
@@ -0,0 +1,14 @@
+package com.lee.rankujp.hotel.mvc.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SitemapService {
+
+
+    public String makeSiteMap() {
+
+    }
+}
