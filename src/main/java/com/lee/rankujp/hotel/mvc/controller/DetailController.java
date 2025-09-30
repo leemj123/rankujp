781f85ac5fcc5f48990183d5bfe66ccc7e6c7383
leemj <leemj@odongdev.com>
2025-09-30 16:18:04 +0900

    사이트맵 준비

--- src/main/java/com/lee/rankujp/hotel/mvc/controller/DetailController.java
+++ src/main/java/com/lee/rankujp/hotel/mvc/controller/DetailController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,10 +15,30 @@ public class DetailController {
 
     private final HotelService hotelService;
 
-    @GetMapping("/hotel/{id}")
-    public String hotel(Model model, @PathVariable Long id, @RequestParam(defaultValue = "0") int top){
+    @GetMapping("/sale/hotel/{id}")
+    public String saleHotel(Model model, @PathVariable Long id, @RequestParam(defaultValue = "0") int top){
         model.addAttribute("top", top);
         model.addAttribute("content", hotelService.HotelDetail(id));
+        model.addAttribute("navValue","할인랭킹");
+        model.addAttribute("navBack","/");
+        return "hotel-detail";
+    }
+
+    @GetMapping("/score/hotel/{id}")
+    public String scoreHotel(Model model, @PathVariable Long id, @RequestParam(defaultValue = "0") int top){
+        model.addAttribute("top", top);
+        model.addAttribute("content", hotelService.HotelDetail(id));
+        model.addAttribute("navValue","종합랭킹");
+        model.addAttribute("navBack","/score");
+        return "hotel-detail";
+    }
+
+    @GetMapping("/premium/hotel/{id}")
+    public String premiumHotel(Model model, @PathVariable Long id, @RequestParam(defaultValue = "0") int top){
+        model.addAttribute("top", top);
+        model.addAttribute("content", hotelService.HotelDetail(id));
+        model.addAttribute("navValue","프리미엄");
+        model.addAttribute("navBack","/premium");
         return "hotel-detail";
     }
 }
--- src/main/java/com/lee/rankujp/hotel/review/scrapper/AnotherReviewTran.java
+++ src/main/java/com/lee/rankujp/hotel/review/scrapper/AnotherReviewTran.java
@@ -3,6 +3,8 @@ package com.lee.rankujp.hotel.review.scrapper;
 import com.lee.rankujp.hotel.cumtom.ReviewBrand;
 import com.lee.rankujp.hotel.infra.Hotel;
 import com.lee.rankujp.hotel.infra.HotelReview;
+import com.lee.rankujp.hotel.price.dto.ImgStarResponse;
+import com.lee.rankujp.hotel.repo.HotelRepo;
 import com.lee.rankujp.hotel.repo.HotelReviewRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class AnotherReviewTran {
     private final HotelReviewRepo hotelReviewRepo;
+    private final HotelRepo hotelRepo;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 5) // 초단 트랜잭션
     public void insertOne(Hotel hotel, ReviewBrand brand, double score, int reviewCount) {
@@ -28,4 +31,19 @@ public class AnotherReviewTran {
         hotelReviewRepo.saveAndFlush(hr); // 바로 커밋될 트랜잭션이므로 flush OK
         log.info("200");
     }
+
+//    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 5) // 초단 트랜잭션
+//    public void imgUpdate(long id, ImgStarResponse.HotelApiInfo info) {
+//        Hotel h = hotelRepo.findById(id).orElseThrow();
+//
+//        h.imgStarUpdate();
+//        HotelReview hr = HotelReview.builder()
+//                .hotel(hotel)
+//                .allScore(score)
+//                .reviewCount(reviewCount)
+//                .reviewBrand(brand)
+//                .build();
+//
+//        hotelReviewRepo.saveAndFlush(hr);
+//    }
 }
